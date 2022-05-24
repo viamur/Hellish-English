189d949a40cd06c438f67587081b0bd98afa386f
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2022-05-24 20:11:05 +0300

    фикс

---
+++ src/js/slider.js
@@ -0,0 +1,22 @@
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav',
+  prevArrow: $('.slick-prev-js'),
+  nextArrow: $('.slick-next-js'),
+  // appendArrows,
+});
+$('.slider-nav').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false,
+  centerPadding: '0px',
+  infinite: true,
+  variableWidth: false,
+});
