6269d8d4483eebf69a1deb0148d17e2c3e044066
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-05-24 20:55:51 +0300

    add  mob-menu

--- src/js/menu-btn.js
+++ src/js/menu-btn.js
@@ -1,14 +1,20 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const openBtnRef = document.querySelector("[data-menu-open]");
+  const closeBtnRef = document.querySelector("[data-menu-close]");
   const mobileMenuRef = document.querySelector("[data-menu]");
 
-  menuBtnRef.addEventListener("click", () => {
+  openBtnRef.addEventListener("click", () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      openBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    // menuBtnRef.classList.toggle("is-open");
 
+    openBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  closeBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
